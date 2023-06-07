6d490bd36424ffc1f97b0b93e48f9c7b28d80423
CarbonBond <cheeseburgerbbgun@gmail.com>
2023-06-07 10:32:37 -0600

    Seperated show_bytes into multiple functions for each size.

--- section2/show_bytes/show_bytes.c
+++ section2/show_bytes/show_bytes.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
 #include <stdint.h>
 
+int show_bytes_short(short num) {
+  int8_t* bytes = (uint8_t*)&num;
+
+  for (int i = 0; i < sizeof(short); i++) {
+    printf("%02X ", (uint8_t)*bytes);
+    bytes++;
+  }
+
+  printf("\n");
+}
+
+int show_bytes_int(int num) {
+  int8_t* bytes = (uint8_t*)&num;
+
+  for (int i = 0; i < sizeof(int); i++) {
+    printf("%02X ", (uint8_t)*bytes);
+    bytes++;
+  }
+
+  printf("\n");
+}
+
+int show_bytes_double(double num) {
+  int8_t* bytes = (uint8_t*)&num;
+
+  for (int i = 0; i < sizeof(double); i++) {
+    printf("%02X ", (uint8_t)*bytes);
+    bytes++;
+  }
+
+  printf("\n");
+}
+
 int main() {
 
   double num; 
@@ -10,23 +43,14 @@ int main() {
   int8_t* bytes;
 
   if (num < INT16_MAX && num > INT16_MIN) {
-    minRepresentation = sizeof(uint16_t);
-    int16_t number = num;
-    bytes = (int8_t*)&number;
+    show_bytes_short((short)num);
   }
   else if (num < INT32_MAX && num > INT32_MIN){
-    minRepresentation = sizeof(uint32_t);
-    int32_t number = num;
-    bytes = (int8_t*)&number;
+    show_bytes_short((int)num);
   }
   else {
-    minRepresentation = sizeof(double);
-    bytes = (int8_t*)&num;
+    show_bytes_double(num);
   }
-
-  for (int i = 0; i < minRepresentation; i++) {
-    printf("%02X ", (uint8_t)*bytes);
-    bytes++;
-  }
-  printf("\n");
 }
+
+
